6993bfea9e39003009fa7520e79c0af029ac09d5
JunyaoLin-ucsc <jlin232@ucsc.edu>
2025-01-27 20:33:59 -0800

    finished

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
 // Modification's Title: Rocket Patrol: TBD
 // Apprixomate Time: 12 hours
 // Mods I chose: Display the time remaining (in seconds) on the screen (3), Allow the player to control the Rocket after it's fired (1),
-// Continued: Randomize each spaceship's movement direction at the start of each play(Also spaceship movement will randomly choose in the game of each movement) (1)
+// Continued: Randomize each spaceship's movement direction at the start of each play(Also spaceship movement will randomly choose movement after destroy and reset) (1)
 // Continued: Implement mouse cotrol for player movement and left mouse click to fire (5)
 // Continued: Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5), adding 3 seconds when successful hits and subtract 2 seconds when misses.
 // Continued: Create a new enemy Spaceship(totally new art work), that's smaller, faster and worth more points (up to 50 points) (5)
 // Citations: 
-
+// Phaser Pointer Document(No need to cite)
+// Phaser Math, Phaser.Math, Phaser.Math.Angle Official Documents(No need to cite) for randomize spaceship movement
+// ChatGPT to know control the rocket after fire it, to remove the code if (!this.isFiring)...
+// Display Timer: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/timer/ and ChatGPT assist timer match different difficulty
+// Control Timer Add or Subtract: https://gamedev.stackexchange.com/questions/182242/phaser-3-how-to-trigger-an-event-every-1-second, ChatGPT also for assist
+// I created the spaceship artwork and reference Nathan's code to make it faster, smaller and worth more points.
 
 let config = {
     type: Phaser.AUTO,
